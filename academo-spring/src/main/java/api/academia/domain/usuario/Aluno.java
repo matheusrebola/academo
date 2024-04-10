5b55c7fe9ad5791db22558468e884ea66aa45900
Matheus Rebola <matheus.rebola@outlook.com>
2024-04-10 20:38:59 -0300

    first commit

---
+++ academo-spring/src/main/java/api/academia/domain/usuario/Aluno.java
@@ -0,0 +1,18 @@
+package academia.domain.usuario;
+
+import academia.domain.treino;
+import academia.domain.frequencia;
+
+public class Aluno {
+	private String nome;
+	private String sobrenome;
+	private String dataNascimento;
+	private String dataCadastro;
+	private Treino treinoAluno;
+	private Frequencia frequenciaAluno;
+
+	//construtores
+	public Aluno(){
+			
+	}
+}
